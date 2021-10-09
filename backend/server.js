9e16353f316b6a9947150d205de40c686cec027f
irakoze bruce <irabruce20@gmail.com>
2021-10-09 12:56:30 +0200

    ecommerce

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+const express = require("express")
+
+const app = express()
+
+const PORT = process.env.PORT || 5050
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
