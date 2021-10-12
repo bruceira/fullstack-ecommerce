6eac20744b4ae096946f587953799c428b184163
irakoze bruce <irabruce20@gmail.com>
2021-10-12 10:50:51 +0200

    ecommerce

--- frontend/src/components/pages/Cart/Cart.js
+++ frontend/src/components/pages/Cart/Cart.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import './cart.css'
 import CartItem from './CartItem/CartItem'
 
+
 function Cart() {
+  const dispatch = useDispatch()
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
   return (
     <div className="cartscreen">
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
+
+        {
+          cartItems.length === 0 ? (<div>
+            your cart is empty <Link to="/">go back</Link>
+          </div>) : cartItems.map(item => (<CartItem />))
+        }
         <CartItem />
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
-          <p>sabstotal (0) items</p>
+          <p>substotal (0) items</p>
           <p>$3525</p>
         </div>
         <div><button>Proceed to check</button></div>
--- frontend/src/components/pages/Product/Product.js
+++ frontend/src/components/pages/Product/Product.js
@@ -1,41 +1,63 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './product.css'
+import { addToCart } from '../../redux/action/cartAction'
+import { getProductDetails } from '../../redux/action/productAction'
 
-function Product({ imageUrl, name, price, description, productId }) {
+function Product({ match, history }) {
+
+  const [qty, setQty] = useState(1)
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector(state => state.getProductDetails)
+
+  const { loading, error, product } = productDetails
+
+  useEffect(() => {
+    if (product && match.params.id !== product._id) {
+      dispatch(getProductDetails(match.params.id))
+    }
+  }, [match, dispatch, product])
+
+  const addToCarthandler = () => {
+    dispatch(addToCart(product._id, qty))
+    history.push("/cart")
+  }
   return (
     <div className="productscreen">
+      {loading ? (<h2>Loading....</h2>) : error ? (<h2>{error}</h2>) :
+        (<>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
 
-      <div className="productscreen__left">
-        <div className="left__image">
-          <img src={imageUrl} alt={name} />
-        </div>
+            <div className="left__info">
+              <p className="lft__name">{product.name}</p>
+              <p>${product.price}</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
+          <div className='productscreen__right'>
+            <div className="right__info">
+              <p>price:<span>{product.price}</span></p>
+              <p>status:<span>{product.countInStock > 0 ? "In Stock" : "out of Stock "}</span></p>
+              <p>Qty:
+          <select value={qty} onChange={e => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map(x => (<option key={x + 1} value={x + 1}>{x + 1}</option>))}
 
-        <div className="left__info">
-          <p className="lft__name">{name}</p>
-          <p>$554</p>
-          <p>{description}</p>
-        </div>
-      </div>
-      <div className='productscreen__right'>
-        <div className="right__info">
-          <p>price:<span>{price}</span></p>
-          <p>status:<span>in stock</span></p>
-          <p>Qty:
-          <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
+                </select>
 
-          </p>
-          <p>
-            <button type="button"> Add to cart</button>
-          </p>
-        </div>
+              </p>
+              <p>
+                <button type="button" onClick={addToCarthandler}> Add to cart</button>
+              </p>
+            </div>
 
-      </div>
-
+          </div>
+        </>)
+      }
     </div>
   )
 }
